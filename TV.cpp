bf2c6c947b35c2ff543e475aaadc09e22e7c5ecf
hai dong <hai_dong1@yahoo.com>
2014-12-11 12:25:13 +0100

    Create TV.cpp

---
+++ TV.cpp
@@ -0,0 +1,16 @@
+#include "StdAfx.h"
+#include ".\TV.h"
+#using <mscorlib.dll>
+
+using namespace System;
+using namespace System::ComponentModel;
+using namespace System::Collections;
+using namespace System::Windows::Forms;
+using namespace System::Data;
+using namespace System::Drawing;
+
+
+void TV::output()
+{
+	MessageBox::Show("we got a TV.");
+}
