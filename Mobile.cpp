b660bece3d7178c93cae65cf6d41bf41019b4355
hai dong <hai_dong1@yahoo.com>
2014-12-11 12:25:39 +0100

    Create Mobile.cpp

---
+++ Mobile.cpp
@@ -0,0 +1,16 @@
+#include "StdAfx.h"
+#include ".\Mobile.h"
+#using <mscorlib.dll>
+
+using namespace System;
+using namespace System::ComponentModel;
+using namespace System::Collections;
+using namespace System::Windows::Forms;
+using namespace System::Data;
+using namespace System::Drawing;
+
+
+void Mobile::output()
+{
+	MessageBox::Show("We got a Mobile.");
+}
